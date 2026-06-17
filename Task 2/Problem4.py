7268baba9a9f184ef6b063948e417da765a5ec1b
rajinjr619 <rajinjr21@gmail.com>
2026-06-18 00:49:05 +0800

    Add pattern matching to find positions in genome

--- Task 2/Problem4.py
+++ Task 2/Problem4.py
@@ -1,1 +1,11 @@
+pattern = input().strip()
+genome = input().strip()
 
+positions = []
+k = len(pattern)
+
+for i in range(len(genome) - k + 1):
+    if genome[i:i+k] == pattern:
+        positions.append(str(i))
+
+print(" ".join(positions))
