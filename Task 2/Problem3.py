7d3984f985c83d1a9ceaff774c6ea5f6efe29edf
rajinjr619 <rajinjr21@gmail.com>
2026-06-18 00:44:59 +0800

    Add reverse complement functionality for DNA sequences

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,12 @@
+pattern = input().strip()
 
+complement = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C'
+}
+
+reverse_complement = ''.join(complement[n] for n in reversed(pattern))
+
+print(reverse_complement)
